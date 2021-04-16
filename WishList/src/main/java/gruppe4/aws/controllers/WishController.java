e966be29b8af97b5521583be0542182664ab0bad
OliverChrestensen <oliver.chrestensen@hotmail.com>
2021-04-16 10:32:17 +0200

    jell

--- WishList/src/main/java/gruppe4/aws/controllers/WishController.java
+++ WishList/src/main/java/gruppe4/aws/controllers/WishController.java
@@ -1,11 +1,17 @@
 package gruppe4.aws.controllers;
 
+import gruppe4.aws.models.Wish;
+import gruppe4.aws.repository.WishRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WishController {
+    Wish wish = new Wish("", "", "", 0, 0);
+    WishRepository makewish = new WishRepository();
 
     @GetMapping("/Wish")
     public String wish(Model model) {
@@ -13,9 +19,16 @@ public class WishController {
     }
 
     @GetMapping("/registerWish")
-    public String register(){
+    public String register() {
 
         return "registerWish";
+    }
+
+    @PostMapping("/createWish")
+    public String createRegister(@RequestParam("wishName") String wishName, @RequestParam("wishComment") String wishComment, @RequestParam("wishLink") String wishLink, @RequestParam("wishRank") int wishRank, @RequestParam("wishPrice") int wishPrice) {
+        makewish.makeWish(wishName, wishComment, wishLink, wishRank, wishPrice);
+        System.out.println();
+        return "redirect:/wish";
     }
 }
 
--- WishList/src/main/java/gruppe4/aws/repository/WishListRepository.java
+++ WishList/src/main/java/gruppe4/aws/repository/WishListRepository.java
@@ -1,13 +1,5 @@
 package gruppe4.aws.repository;
 
-import gruppe4.aws.models.User;
-import gruppe4.aws.models.Wish;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 
 public class WishListRepository {
 
