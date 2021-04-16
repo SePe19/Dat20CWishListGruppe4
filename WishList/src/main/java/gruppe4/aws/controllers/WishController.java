21a432e917f1b2a740da23130c54daee750e479f
OliverChrestensen <oliver.chrestensen@hotmail.com>
2021-04-16 10:02:25 +0200

    hej

--- WishList/src/main/java/gruppe4/aws/controllers/WishController.java
+++ WishList/src/main/java/gruppe4/aws/controllers/WishController.java
@@ -1,4 +1,14 @@
 package gruppe4.aws.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class WishController {
+
+    @GetMapping("/Wish")
+    public String wish(Model model)
+        return "/wish";
+
 }
