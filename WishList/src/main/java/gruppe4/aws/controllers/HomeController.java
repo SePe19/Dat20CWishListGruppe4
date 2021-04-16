73fb7f259809c4f89cbbacc47871e4fd67a11ae7
SePe19 <72380247+SePe19@users.noreply.github.com>
2021-04-16 14:38:27 +0200

    HomeController

--- WishList/src/main/java/gruppe4/aws/controllers/HomeController.java
+++ WishList/src/main/java/gruppe4/aws/controllers/HomeController.java
@@ -1,11 +1,11 @@
 package gruppe4.aws.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
     @GetMapping("/frontPage")
     public String frontPage() {
         return "frontPage.html";
